dcc450c2ee7afdc6dc85afe82fb46c834d815be6
N. Navina Nanthini <navinajeon@gmail.com>
2026-03-19 10:27:55 +0530

    Refactor LoginSignup component and update imports

--- LoginSignup.jsx
+++ LoginSignup.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-import './LoginSignup.css';
+import 'LoginSignup.css';
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/lock.png'
+import user_icon from 'person.png'
+import email_icon from 'email.png'
+import password_icon from 'lock.png'
 
 const LoginSignup = () => {
 
